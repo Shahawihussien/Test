2b249dedf378d8bc4736b728bea2bb61b856d0ce
Hussien <hussien_amr@icloud.com>
2023-07-14 15:28:48 +0300

    Initial Commit

---
+++ Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by Hussien Shahawi on 30/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
